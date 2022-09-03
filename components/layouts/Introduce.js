bc2419807776559cf472152477f886960c0bb7df
sanghoonseo <summit1991@naver.com>
2022-09-03 17:05:51 +0900

    소개페이지 레이아웃 구성

--- components/layouts/Introduce.js
+++ components/layouts/Introduce.js
@@ -1,8 +1,47 @@
+import { Fragment } from "react";
+import { Box, Card, CardContent, CardMedia, Container, IconButton, Typography } from "@mui/material";
+
 export default function Introduce({ children }) {
   return (
-    <div>
-      <div>이거 너무한 거 아니냐고 sipal</div>
-      <div>{children}</div>
-    </div>
+    <Fragment>
+      <Container fixed sx={{bgcolor: '#cfe8fc'}}>
+        <header>
+          <Card sx={{ display: 'flex' }} elevation={0}>
+            <CardMedia
+              component="img"
+              sx={{ width: 250 }}
+              image="/profile_image.jpeg"
+              alt="Live from space album cover"
+            />
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <CardContent sx={{ flex: '1 0 auto' }}>
+                <Typography component="div" variant="h5">
+                  Live From Space
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary" component="div">
+                  Mac Miller
+                </Typography>
+              </CardContent>
+            </Box>
+          </Card>
+        </header>
+        <main>
+          <section>
+            <article></article>
+            <article></article>
+          </section>
+
+          <section>
+            <section>
+              <article></article>
+              <article></article>
+            </section>
+            <section>
+              <article></article>
+            </section>
+          </section>
+        </main>
+      </Container>
+    </Fragment>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-import Introduce from "../components/layouts/Introduce";
+import Introduce from "../components/layouts/Introduce"
+import Button from '@mui/material/Button'
 
 export default function Index() {
   return (
-    <div>인덱스가 바로 소개페이지로 쓰이는 것이지 말입니다.</div>
+    <div>
+      <div>인덱스가 바로 소개페이지로 쓰이는 것이지 말입니다.</div>
+      <Button variant="contained">이거 너무한 거 아니냐고</Button>
+    </div>
   )
 }
 
